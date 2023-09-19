314ed43c80bc60192811afa9de17479cca510e8d
Haider Ali <138826274+Haiderally@users.noreply.github.com>
2023-09-19 19:15:16 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 < 1);
+// console.log(9 >= 5);
+// console.log(2 == 1);
+// console.log(7 != 7);
+// console.log(7 == "7");
+// console.log(7 === "7");
+
+// console.log(null < 0);   //false
+// console.log(null > 0);   //false
+// console.log(null <= 0);  //true
+// console.log(null >= 0);  //true
+
+console.log(undefined == 0); //false  - Whenever we compare undefined with a value it always return false.
+console.log(undefined > 0);  //false 
+console.log(undefined < 0);  //false 
+
