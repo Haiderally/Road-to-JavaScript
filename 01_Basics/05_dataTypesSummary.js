c9b7a571a3f58c56f462f6f4b258ca60d3674853
Haider Ali <138826274+Haiderally@users.noreply.github.com>
2023-09-20 07:29:46 +0000

    stack and heap memory

--- 01_Basics/05_dataTypesSummary.js
+++ 01_Basics/05_dataTypesSummary.js
@@ -33,6 +33,39 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-// console.log(typeof anotherId);
+console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ****************************************************
+
+// *****stack (memory use for primitive dataTypes) *****//
+
+
+//example of heap memory
+let mysiteName = "haiderallydotcom"
+
+let yoursiteName = mysiteName
+yoursiteName = "aally.com"
+
+// console.log(mysiteName)
+// console.log(yoursiteName)
+
+
+// **** heap (memory use for non-primitive dataTypes)******//
+
+
+//example of heap memory
+let userOne = {
+    email: "myemail.com",
+    idNumber: "6464768548",                         
+}
+
+let userTwo = userOne
+
+userTwo.email = "haiderapparels@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
