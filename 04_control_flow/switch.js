3f5b7fd5122be24a39319560c22fac6d80f8e880
Haider Ali <138826274+Haiderally@users.noreply.github.com>
2023-09-24 16:59:11 +0000

    switch case statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,64 @@
+//  Basic Syntax of switch case statement 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+
+// }
+
+
+// ----------------------------
+// const month = 3;      //if we want to check for numbers we use this syntax
+
+// switch (month) {
+//     case 1:
+//         console.log('jan')
+//         break;
+//     case 2:
+//         console.log('feb')
+//         break;
+//     case 3:
+//         console.log('march')
+//         break;
+//     case 4:
+//         console.log('april')
+//         break;
+//     case 5:
+//         console.log('may')
+//         break;
+    
+//     default:
+//         console.log(Default);
+//         break;
+// }
+// }
+
+
+// -----------------------------------------
+const month = 'april';      //if we want to check for strings we use this syntax
+
+switch (month) {
+    case 'jan':
+        console.log('jan')
+        break;
+    case 'feb':
+        console.log('feb')
+        break;
+    case 'march':
+        console.log('march')
+        break;
+    case 'april':
+        console.log('april')
+        break;
+    case 'may':
+        console.log('may')
+        break;
+    
+    default:
+        console.log(Default);
+        break;
+}
