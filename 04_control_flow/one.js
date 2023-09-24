5a394be4fad1a2baa3f887fc422ed85eb40bb534
Haider Ali <138826274+Haiderally@users.noreply.github.com>
2023-09-24 16:37:52 +0000

    Conditions in Js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,64 @@
+if (true) {
+    // if the condition is true the code will execute
+}
+
+if (false) {
+    // if the condition is false the code will not execute
+}
+
+//------------------
+// const score = 300;
+
+// if (score < 400) {
+//     const position = '1st'
+//     console.log(`user score: ${position}`)
+// }
+
+
+
+// ---------------
+// const value = 5000;
+
+// if (value > 500){
+//     console.log("your value is greater then 500");
+// }
+
+// else if (value > 900){
+//     console.log("your value is greater then 900");
+// }
+
+// else if (value > 1200){
+//     console.log("your value is greater then 1200");
+// }
+// else {
+//     console.log("your value is greater than 1500")
+// }
+
+
+// ---------- if we want to check more then one conditons in 'if statement' we use &&  ---e.g....
+// if we are using '&&' in any if statement then all the conditions must be true for code execution--Note: if atleast only one statement is false then the code will not execute
+
+// const isLoggedIN = true;
+// const debitCard = true;
+// // const debitCard = false;
+
+// if (isLoggedIN && debitCard && 2==2) {
+//     console.log("You have logged in and also carring debit card now you can buy the course.");
+// }
+// else {
+    //     console.log("You have not logged in or not carring debit card that's why  you can't buy the course.");
+// }
+
+
+
+// ---------------------
+// ---------- if we want to check more then one conditons in 'if statement' we also use || . Note: in this cause if alleast one is true the code wil execute   ---e.g....
+
+// const isLoggedINfromGoogle = true
+// // const isLoggedINfromEmail = true
+// const isLoggedINfromEmail = false
+
+// if (isLoggedINfromGoogle || isLoggedINfromEmail){
+//     console.log("you are log in .")
+// }
+
