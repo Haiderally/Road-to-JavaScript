a45982f69acdaa409d7fb01889fbf6b131d0a148
Haider Ali <138826274+Haiderally@users.noreply.github.com>
2023-09-24 22:22:12 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// let index = 0;
+
+// while (index <= 10) {
+//     console.log(`value of index is : ${index}`);
+//     index++
+// //     index = index + 2
+// }
+
+// ----------------------------while loop for array 
+
+// const myArray = ['superman', 'anpanman', 'krish']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is : ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// ---------------- do while loop
+     //do while loop execute atleast one time rather the condition may be false
+
+// // let score = 1;
+// let score = 15;
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10);
